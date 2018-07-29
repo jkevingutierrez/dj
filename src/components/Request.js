99a6ec2598b85a0df4b1fa94546343b496b1a2fc
Kevin Gutiérrez <>
2018-07-29 12:48:24 -0500

    Add requests list

---
+++ src/components/Request.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  }
+});
+
+class Request extends Component {
+  render() {
+    const { classes, date, location, type, handleAccept, handleReject } = this.props;
+    return (
+      <ListItem>
+        <ListItemText
+          primary = {
+            `Solicitud de ${type} en ${location.charAt(0).toUpperCase() + location.slice(1).toLowerCase()} para ${date}`
+          }
+        />
+      <ListItemSecondaryAction>
+        <Button variant="contained" color="primary" className={classes.button} onClick={handleAccept}>
+          Aceptar
+        </Button>
+        <Button variant="contained" color="secondary" className={classes.button} onClick={handleReject}>
+          Rechazar
+        </Button>
+      </ListItemSecondaryAction>
+    </ListItem>
+    );
+  }
+}
+
+export default withStyles(styles)(Request);
