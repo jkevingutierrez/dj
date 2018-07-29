2f0dc6fbea5659ddbc557baa1064de5d5cafd535
Kevin Gutiérrez <>
2018-07-29 07:48:39 -0500

    Fix composeEnhancer

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,7 +6,7 @@ export default () => {
   const composeEnhancers =
     typeof window === 'object' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
       : compose;
 
   const store = createStore(
