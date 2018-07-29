dde2197c11a5643e53b5d85ed7fe551240986f96
Kevin Gutiérrez <>
2018-07-29 07:28:00 -0500

    Add firebase deploy

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -12,13 +12,18 @@ import thunk from 'redux-thunk';
 // import parameters from '../reducers/parameters';
 
 export default () => {
+  const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+      : compose;
+
   const store = createStore(
     combineReducers({
       // ...
     }),
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+      applyMiddleware(thunk)
     )
   );
 
