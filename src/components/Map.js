46dd5e35b5b12fdd1f2e6eaba2d6edad1ca304a2
Arthur Picerna <arthurpicerna@gmail.com>
2018-07-29 06:55:17 -0500

    interactive locality shapes

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import { success as mapSuccess } from '../actions/map';
@@ -15,29 +16,54 @@ const GOOGLE_MAPS_API_KEY = 'AIzaSyBYuNcKCVqlkr-le0gxLDYwq-EWBt5Jbvg';
 export class MapComponent extends Component {
   state = {
     showingInfoWindow: false,
+    positionInfoWindow: null,
     activeMarker: {},
     selectedPlace: {}
   };
 
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    });
+  // onMarkerClick = (props, marker, e) => {
+  //   this.setState({
+  //     selectedPlace: props,
+  //     showingInfoWindow: true
+  //   });
+  // };
 
   onMapClicked = (props) => {
     if (this.state.showingInfoWindow) {
+      this.state.selectedPlace._feature.setProperty('isClicked', false);
       this.setState({
         showingInfoWindow: false,
-        activeMarker: null
+        selectedPlace: {}
       });
     }
   };
 
-  onMapDragend(mapProps, map) {
+  onMapDragend = (mapProps, map) => {
     // ...
-  }
+  };
+
+  onInfoWindowClose = (...args) => {
+    if (this.state.showingInfoWindow) {
+      this.state.selectedPlace._feature.setProperty('isClicked', false);
+      this.setState({
+        showingInfoWindow: false,
+        selectedPlace: {}
+      });
+    }
+  };
+
+  renderInfoWindowContent = () => {
+    ReactDOM.render(
+      <div>
+        <button onClick={this.onInfoWindowButtonClick}>Seleccionar</button>
+      </div>,
+      document.getElementById('iw-content')
+    );
+  };
+
+  onInfoWindowButtonClick = (event) => {
+    console.log('onInfoWindowButtonClick()', event);
+  };
 
   onReady = (mapProps, map) => {
     // const { google } = mapProps;
@@ -51,6 +77,12 @@ export class MapComponent extends Component {
       let fillOpacity;
       let zIndex;
       if (feature.getProperty('isOver')) {
+        fillOpacity = 0.5;
+        strokeWeight = 2;
+        zIndex = 999;
+      }
+      if (feature.getProperty('isClicked')) {
+        fillColor = '#0097a7';
         fillOpacity = 0.7;
         strokeWeight = 4;
         zIndex = 999;
@@ -68,6 +100,23 @@ export class MapComponent extends Component {
     });
     map.data.addListener('mouseout', (event) => {
       event.feature.setProperty('isOver', false);
+    });
+    map.data.addListener('click', (event) => {
+      if (this.state.selectedPlace._feature !== void 0) {
+        this.state.selectedPlace._feature.setProperty('isClicked', false);
+      }
+      event.feature.setProperty('isClicked', true);
+      this.setState(
+        () => ({
+          showingInfoWindow: true,
+          positionInfoWindow: event.latLng,
+          selectedPlace: {
+            name: event.feature.getProperty('NOMBRE'),
+            _feature: event.feature
+          }
+        }),
+        this.renderInfoWindowContent
+      );
     });
   };
 
@@ -87,9 +136,15 @@ export class MapComponent extends Component {
         onReady={this.onReady}
       >
         {/* <Marker onClick={this.onMarkerClick} name={'Current location'} /> */}
-        <InfoWindow marker={this.state.activeMarker} visible={this.state.showingInfoWindow}>
+        <InfoWindow
+          // marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          position={this.state.positionInfoWindow}
+          onClose={this.onInfoWindowClose}
+        >
           <div>
-            <h1>{this.state.selectedPlace.name}</h1>
+            <h3>{this.state.selectedPlace.name}</h3>
+            <div id="iw-content" />
           </div>
         </InfoWindow>
       </Map>
